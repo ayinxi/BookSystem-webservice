bfb4bd112ec1ebf193df516fafbd37160b2523bb
destinyvoilet <2516795059@qq.com>
2021-07-12 14:02:02 +0800

    token

---
+++ src/main/java/booksystem/interceptor/LoginFilter.java
@@ -0,0 +1,33 @@
+package booksystem.interceptor;
+
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class LoginFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        System.out.println("TestFilter,"+request.getRequestURI());
+
+        //执行
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/booksystem/utils/TokenUtils.java
@@ -0,0 +1,74 @@
+package booksystem.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class TokenUtils {
+    //设置过期时间
+    private static final long EXPIRE_DATE=30*60*100000;
+    //token秘钥
+    private static final String TOKEN_SECRET = "ZCBOOKBFKSYSTEM2021BQWE";
+
+    public static String generateToken (String username,String password){
+
+        String token = "";
+        try {
+            //过期时间
+            Date date = new Date(System.currentTimeMillis()+EXPIRE_DATE);
+            //秘钥及加密算法
+            Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
+            //设置头部信息
+            Map<String,Object> header = new HashMap<>();
+
+            header.put("typ","JWT");
+            header.put("alg","HS256");
+            //携带username，password信息，生成签名
+            token = JWT.create()
+                    .withHeader(header)
+                    .withClaim("username",username)
+                    .withClaim("password",password).withExpiresAt(date)
+                    .sign(algorithm);
+        }catch (Exception e){
+            e.printStackTrace();
+            return  null;
+        }
+        return token;
+    }
+
+    public static boolean verify(String token){
+        /**
+         * @desc   验证token，通过返回true
+         * @create 2021/7/12 10:39
+         * @params [token]需要校验的串
+         **/
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT jwt = verifier.verify(token);
+            String username=jwt.getClaim("username").asString();
+            System.out.println(username);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return  false;
+        }
+    }
+
+    public static Objects parseToken
+    public static void main(String[] args) {
+        String username ="zhangsan";
+        String password = "123";
+        String token = generateToken(username,password);
+        System.out.println(token);
+//        token+=;
+        boolean b = verify(token);
+        System.out.println(b);
+    }
+}
