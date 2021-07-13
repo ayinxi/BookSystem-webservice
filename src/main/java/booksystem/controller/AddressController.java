290b3ec9a0a0ba657f2449a1e90292fd43a5fd32
shichuanhe <1151387549@qq.com>
2021-07-13 18:50:57 +0800

    实现了user的controller和mapper

--- src/main/java/booksystem/controller/AddressController.java
+++ src/main/java/booksystem/controller/AddressController.java
@@ -15,42 +15,42 @@ import java.util.List;
 @CrossOrigin(origins="*",maxAge = 3600)
 @RestController
 public class AddressController {
-    @Autowired
-    AddressService addressService;
-
-    //根据user_id获取所有地址
-    @RequestMapping("/user/getAllAddress")
-    public Result getAllAddress(String user_id){
-        return Result.ok().put("data",addressService.getAllAddress(user_id));
-    }
-    //添加一个地址
-    @RequestMapping("/user/addAddress")
-    public Result addAddress(Address address){
-        int result= addressService.addAddress(address);
-        if(result>0)
-        {
-            return Result.ok("操作成功");
-        }
-        return Result.ok("操作失败");
-    }
-    //删除一个地址
-    @RequestMapping("/user/deleteAddress")
-    public Result deleteAddress(String address_id){
-        int result= addressService.deleteAddress(address_id);
-        if(result>0)
-        {
-            return Result.ok("操作成功");
-        }
-        return Result.ok("操作失败");
-    }
-    //修改地址信息
-    @RequestMapping("/user/updateAddress")
-    public Result updateAddress(Address address){
-        int result= addressService.updateAddress(address);
-        if(result>0)
-        {
-            return Result.ok("操作成功");
-        }
-        return Result.ok("操作失败");
-    }
+//    @Autowired
+//    AddressService addressService;
+//
+//    //根据user_id获取所有地址
+//    @RequestMapping("/user/getAllAddress")
+//    public Result getAllAddress(String user_id){
+//        return Result.ok().put("data",addressService.getAllAddress(user_id));
+//    }
+//    //添加一个地址
+//    @RequestMapping("/user/addAddress")
+//    public Result addAddress(Address address){
+//        int result= addressService.addAddress(address);
+//        if(result>0)
+//        {
+//            return Result.ok("操作成功");
+//        }
+//        return Result.ok("操作失败");
+//    }
+//    //删除一个地址
+//    @RequestMapping("/user/deleteAddress")
+//    public Result deleteAddress(String address_id){
+//        int result= addressService.deleteAddress(address_id);
+//        if(result>0)
+//        {
+//            return Result.ok("操作成功");
+//        }
+//        return Result.ok("操作失败");
+//    }
+//    //修改地址信息
+//    @RequestMapping("/user/updateAddress")
+//    public Result updateAddress(Address address){
+//        int result= addressService.updateAddress(address);
+//        if(result>0)
+//        {
+//            return Result.ok("操作成功");
+//        }
+//        return Result.ok("操作失败");
+//    }
 }
